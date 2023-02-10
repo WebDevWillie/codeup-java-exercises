08c39f178ef31b86a9688739b589b16bdef84bbb
Will Parry <wsparry99@gmail.com>
2023-02-10 10:52:07 -0600

    Java exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,27 +7,31 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i++;
 //        }
-        for(int i = 5; i <= 15; i++){
-            System.out.println(i);
-        }
+//        for(int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
 
-        for(int z = 0; z <= 100; z+=2){
-            System.out.println(z);
-        }
+//        for(int z = 0; z <= 100; z+=2){
+//            System.out.println(z);
+//        }
 
 
-
+//        for(int x = 100; x >= -10; x-=5){
+//            System.out.println(x);
+//        }
 //        do {
 //            System.out.println(i);
 //            i -= 5;
 //        } while (i >= -10);
 
+//        for(long c = 2; c < 1000000; c*=c){
+//            System.out.println(c);
+//        }
 
-
-        do {
-            System.out.println(i);
-            i *= i;
-        } while (i < 1000000);
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
 
     }
 
